28484cabbf5fbb24f76170ab38b45eb1dfc9ad99
Rimantas <belauzas@gmail.com>
2023-05-19 10:47:04 +0300

    Initial content

---
+++ js/pages/home.js
@@ -0,0 +1,36 @@
+// IMPORTS
+
+// EXECUTION
+
+/* HEADER: start */
+/* HEADER: end */
+
+/* HERO: start */
+/* HERO: end */
+
+/* ABOUT ME: start */
+/* ABOUT ME: end */
+
+/* SERVICES: start */
+/* SERVICES: end */
+
+/* RESUME: start */
+/* RESUME: end */
+
+/* FREELANCE: start */
+/* FREELANCE: end */
+
+/* PORTFOLIO: start */
+/* PORTFOLIO: end */
+
+/* TESTIMONIALS: start */
+/* TESTIMONIALS: end */
+
+/* BLOGS: start */
+/* BLOGS: end */
+
+/* CONTACT: start */
+/* CONTACT: end */
+
+/* FOOTER: start */
+/* FOOTER: end */
