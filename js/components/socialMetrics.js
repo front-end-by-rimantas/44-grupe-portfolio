8d70e7640a28356c06936982c780026d08654179
Rimantas <belauzas@gmail.com>
2023-06-01 11:47:52 +0300

    Scroll event

--- js/components/socialMetrics.js
+++ js/components/socialMetrics.js
@@ -1,5 +1,13 @@
 // return -> error-first
 
+function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    const x = window.innerWidth || document.documentElement.clientWidth;
+    const y = window.innerHeight || document.documentElement.clientHeight;
+
+    return rect.bottom > 0 && rect.right > 0 && rect.left < x && rect.top < y;
+}
+
 function socialMetrics(selector, data) {
     if (typeof selector !== 'string' || selector === '') {
         return [true, 'ERROR: selector has to be non-empty string'];
@@ -14,6 +22,7 @@ function socialMetrics(selector, data) {
         return [true, 'ERROR: data has to be non-empty array'];
     }
 
+    // Generuojame turini
     let HTML = '';
 
     for (const item of data) {
@@ -25,6 +34,24 @@ function socialMetrics(selector, data) {
 
     DOM.classList.add('social-metrics');
     DOM.innerHTML = HTML;
+
+    // Stebim scroll'a
+    const metricsDOM = DOM.querySelectorAll('.metric');
+    const animatedMetrics = new Array(metricsDOM.length).fill(false);
+
+    window.addEventListener('scroll', () => {
+        let i = -1;
+        for (const metricDOM of metricsDOM) {
+            i++;
+            if (animatedMetrics[i]) {
+                continue;
+            }
+            if (isElementInViewport(metricDOM)) {
+                animatedMetrics[i] = true;
+                console.log('animuojame:', i);
+            }
+        }
+    });
 
     return [false, 'OK'];
 }
